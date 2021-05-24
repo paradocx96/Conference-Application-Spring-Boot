5153412951726d229cd281336be25be488892215
paradocx96 <adilanga@gmail.com>
2021-05-24 11:34:15 +0530

    KeyNote Basic CRUD

--- src/main/java/com/rhna/conference/controller/KeyNoteEndpoint.java
+++ src/main/java/com/rhna/conference/controller/KeyNoteEndpoint.java
@@ -3,10 +3,14 @@ package com.rhna.conference.controller;
 import com.rhna.conference.api.KeyNoteApi;
 import com.rhna.conference.domain.KeyNote;
 import com.rhna.conference.dto.KeyNoteDto;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*", exposedHeaders = "*")
 @RestController
 @RequestMapping("/keynote")
 public class KeyNoteEndpoint {
@@ -29,6 +33,32 @@ public class KeyNoteEndpoint {
         keyNote.setUser(keyNoteDto.getUser());
 
         return keyNoteApi.addKeyNote(keyNote);
+    }
+
+    @GetMapping
+    public List<KeyNote> getKeyNotes(){
+        return keyNoteApi.getKeyNotes();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteKeyNoteById(@PathVariable String id) {
+        keyNoteApi.deleteKeyNote(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public KeyNote updateKeyNote(@RequestBody KeyNoteDto keyNoteDto, @PathVariable String id) {
+        KeyNote keyNote = new KeyNote();
+
+        keyNote.setId(id);
+        keyNote.setSpeakername(keyNoteDto.getSpeakername());
+        keyNote.setSpeakertype(keyNoteDto.getSpeakertype());
+        keyNote.setOrganization(keyNoteDto.getOrganization());
+        keyNote.setDescription(keyNoteDto.getDescription());
+        keyNote.setUser(keyNoteDto.getUser());
+
+        return keyNoteApi.updateKeyNote(keyNote);
     }
 
 }
--- src/main/java/com/rhna/conference/domain/KeyNoteDataAdapter.java
+++ src/main/java/com/rhna/conference/domain/KeyNoteDataAdapter.java
@@ -1,10 +1,17 @@
 package com.rhna.conference.domain;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface KeyNoteDataAdapter {
 	
 	KeyNote save(KeyNote keyNote);
 
-	List<KeyNote> getall();
+	List<KeyNote> getAll();
+
+	void deleteById(String id);
+
+	KeyNote update(KeyNote keyNote);
+
+	Optional<KeyNote> getKeyNoteById(String id);
 }
