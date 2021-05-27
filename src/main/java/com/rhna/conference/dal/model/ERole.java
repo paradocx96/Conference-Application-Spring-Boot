cdf5126c5afe4288ced2a8c1501ae687545cd70c
DeclanChirush <hirushgimhan2016@gmail.com>
2021-05-28 00:43:52 +0530

    Created user models

---
+++ src/main/java/com/rhna/conference/dal/model/ERole.java
@@ -0,0 +1,9 @@
+package com.rhna.conference.dal.model;
+
+//Create enum class to create instance of each enum constants
+public enum ERole {
+	ROLE_USER,
+	ROLE_REVIEWER,
+	ROLE_EDITOR,
+    ROLE_ADMIN
+}
