1b9f53608e39fcc5aca8fe86e4aa32ce0fcef05e
Amarangi Karunathilaka <amarangikarunathilaka@gmail.com>
2021-05-29 18:48:38 +0530

    create unit test for update, delete, getScheduled, getAllPending workshops.

--- src/test/java/com/rhna/conference/dal/adapter/WorkshopAdapterMongoImplTest.java
+++ src/test/java/com/rhna/conference/dal/adapter/WorkshopAdapterMongoImplTest.java
@@ -4,11 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rhna.conference.RhnaConferenceBackendApplicationTests;
 import com.rhna.conference.dal.model.WorkshopModel;
 import com.rhna.conference.dal.repository.WorkshopMongoRepository;
+import com.rhna.conference.domain.Workshop;
 import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.HttpEntity;
+
+import java.util.List;
 
 public class WorkshopAdapterMongoImplTest extends RhnaConferenceBackendApplicationTests {
 
@@ -64,7 +68,6 @@ public class WorkshopAdapterMongoImplTest extends RhnaConferenceBackendApplicati
             for (WorkshopModel workshopModel1 : workshopModels) {
                 workshopMongoRepository.save(workshopModel1);
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -72,14 +75,109 @@ public class WorkshopAdapterMongoImplTest extends RhnaConferenceBackendApplicati
 
     @AfterEach
     void tearDown() {
+        workshopMongoRepository.deleteById("workshopId_01");
+        workshopMongoRepository.deleteById("workshopId_02");
     }
 
+    @Test
+    @DisplayName("Test should pass the workshop each fields values going equal")
+    void testSave() {
+        Workshop newWorkshop = new Workshop();
+        newWorkshop.setId("workshopId_03");
+        newWorkshop.setUsername("dummyUserName");
+        newWorkshop.setTitle("dummyTitle");
+        newWorkshop.setCourseCode("dummyCourseCode");
+        newWorkshop.setVenue("dummyVenue");
+        newWorkshop.setDate("dummyDate");
+        newWorkshop.setStartingTime("dummyStartingTime");
+        newWorkshop.setEndTime("dummyEndTime");
+        newWorkshop.setDescription("dummyDescription");
+        newWorkshop.setDocuments(null);
 
+        Workshop result = workshopAdapterMongo.save(newWorkshop);
+
+        Assertions.assertEquals(result.getId(), "workshopId_03");
+        Assertions.assertEquals(result.getUsername(), "dummyUserName");
+        Assertions.assertEquals(result.getTitle(), "dummyTitle");
+        Assertions.assertEquals(result.getCourseCode(), "dummyCourseCode");
+        Assertions.assertEquals(result.getVenue(), "dummyVenue");
+        Assertions.assertEquals(result.getDate(), "dummyDate");
+        Assertions.assertEquals(result.getStartingTime(), "dummyStartingTime");
+        Assertions.assertEquals(result.getEndTime(), "dummyEndTime");
+        Assertions.assertEquals(result.getDescription(), "dummyDescription");
+        Assertions.assertNull(result.getDocuments());
+        Assertions.assertEquals(result.getHasDocuments(), false);
+        workshopAdapterMongo.delete("workshopId_03"); // Remove the document from database.
+    }
 
     @Test
-    void approve() {
-        WorkshopAdapterMongoImpl workshopAdapterMongo = new WorkshopAdapterMongoImpl(workshopMongoRepository, mongoTemplate);
-        Object result = workshopAdapterMongo.approve("workshopId_01");
+    void testGetAll() {
+        List<Workshop> results = workshopAdapterMongo.getAll();
+//        Assertions.assertEquals(results.size(), 2);
+    }
+
+    @Test
+    void testGetFileById() {
+        HttpEntity<byte[]> result = workshopAdapterMongo.getFileById("workshopId_02");
+        Assertions.assertEquals(result, null);
+    }
+
+    @Test
+    void testUpdate() {
+
+        //Create a dummy updated field workshop with existing workshop id.
+        Workshop existingWorkshop = new Workshop();
+        existingWorkshop.setId("workshopId_01");
+        existingWorkshop.setUsername("updatedUserName");
+        existingWorkshop.setTitle("updatedTitle");
+        existingWorkshop.setCourseCode("updatedCourseCode");
+        existingWorkshop.setVenue("updatedVenue");
+        existingWorkshop.setDate("updatedDate");
+        existingWorkshop.setStartingTime("updatedStartingTime");
+        existingWorkshop.setEndTime("updatedEndTime");
+        existingWorkshop.setDescription("updatedDescription");
+        existingWorkshop.setDocuments(null);
+
+        Workshop result = workshopAdapterMongo.update(existingWorkshop);
+
+        Assertions.assertEquals(result.getId(), "workshopId_01");
+        Assertions.assertEquals(result.getUsername(), "updatedUserName");
+        Assertions.assertEquals(result.getTitle(), "updatedTitle");
+        Assertions.assertEquals(result.getCourseCode(), "updatedCourseCode");
+        Assertions.assertEquals(result.getVenue(), "updatedVenue");
+        Assertions.assertEquals(result.getDate(), "updatedDate");
+        Assertions.assertEquals(result.getStartingTime(), "updatedStartingTime");
+        Assertions.assertEquals(result.getEndTime(), "updatedEndTime");
+        Assertions.assertEquals(result.getDescription(), "updatedDescription");
+        Assertions.assertNull(result.getDocuments());
+        Assertions.assertEquals(result.getHasDocuments(), false);
+    }
+
+    @Test
+    void testGetAllPending() {
+        List<Workshop> results = workshopAdapterMongo.getAllPending();
+        for (Workshop result : results) {
+            Assertions.assertEquals(result.getIsPublished(), false);
+        }
+    }
+
+    @Test
+    void getAllScheduled() {
+        List<Workshop> results = workshopAdapterMongo.getAllScheduled();
+        for (Workshop result : results) {
+            Assertions.assertEquals(result.getIsPublished(), true);
+        }
+    }
+
+    @Test
+    void testDelete() {
+        String result = workshopAdapterMongo.delete("workshopId_01");
+        Assertions.assertEquals(result, "Deleted");
+    }
+
+    @Test
+    void testApprove() {
+        String result = workshopAdapterMongo.approve("workshopId_01");
         Assertions.assertEquals(result, "Approved");
     }
 }
