7a32467c759f6169e9f828a2c6fd08b8ec9c49ad
Navinda Lankesh <adilanga@gmail.com>
2021-05-30 15:40:32 +0530

    Comments Updated About

--- src/main/java/com/rhna/conference/api/AboutApi.java
+++ src/main/java/com/rhna/conference/api/AboutApi.java
@@ -19,25 +19,37 @@ public class AboutApi {
         this.adapter = adapter;
     }
 
+    // Save about details method
     public About saveAbout(About about) {
+        // Assign current date and time to about object
         about.setDatetime(LocalDateTime.now());
+
+        // Calling saveAbout method in Adapter class and return value
         about = adapter.saveAbout(about);
         return about;
     }
 
+    // Getting all about method
     public List<About> getAllAbout(){
+        // Getting values as array list
         return new ArrayList<>(adapter.getAllAbout());
     }
 
+    // About details get by id method
     public List<About> getByIdAbout(String id) {
+        // calling getbyid method using parameter as id
         return adapter.getByIdAbout(id);
     }
 
+    // Delete about details by id method
     public void deleteByIdAbout(String id) {
+        // Calling delete method, id as parameter
         adapter.deleteByIdAbout(id);
     }
 
+    // Update about method
     public About updateAbout(About about) {
+        // Calling update method passing about object
         return adapter.updateAbout(about);
     }
 }
